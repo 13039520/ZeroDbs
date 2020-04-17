f4c218591b2ad719aeb8da76b0896a634bfecd90
13039520 <735753456@qq.com>
2020-04-17 12:40:22 +0800

    edit

--- Huanr.NativeSoilWebApp/wwwroot/huanr/javascript/admin.js
+++
@@ -1,8 +0,0 @@
-﻿zero.ready(function () {
-    zeroAdminUI.mainPage.init();
-    var currentTime = zero('currentTime');
-    if (currentTime.length !== 1) { return; }
-    setInterval(function () {
-        currentTime.html(zero.datetimeFormat(new Date(), 'yyyy年MM月dd日 week HH时mm分ss秒'));
-    }, 1000);
-});
